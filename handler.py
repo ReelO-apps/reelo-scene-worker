8d5cad61cf7b72cfa1d5b70cd47ee83032091aa6
Kenneth Affleck <kenaffleck@gmail.com>
2026-04-27 06:05:56 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,1 +1,10 @@
+import runpod
 
+def handler(job):
+    job_input = job.get("input", {})
+    return {
+        "ok": True,
+        "echo": job_input,
+    }
+
+runpod.serverless.start({"handler": handler})
